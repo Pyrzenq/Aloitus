10ab5e7d98cab2c637e1b72eeb05a4114883a1f2
Pyry <111853294+Pyrzenq@users.noreply.github.com>
2023-01-20 14:31:32 +0200

    .

--- fitness.py
+++ fitness.py
@@ -4,15 +4,16 @@
 # Muuttujat
 
 # Kysytään käyttäjältä tiedot
-pituus_teksti = input('Kuinka pitkä olet (cm): ') # Kysytään pituus
-paino_teksti = input('Kuinka paljon painat (kg): ') # Kysytään paino
-ika_teksti = input('Kuinka vanha olet: ') # Kysytään ikä
-sukupuoli_teksti = input('Sukupuoli mies vastaa 1 ja nainen vastaa 0: ') # Kysytään sukupuoli
+pituus_teksti = input('Kuinka pitkä olet (cm): ')  # Kysytään pituus
+paino_teksti = input('Kuinka paljon painat (kg): ')  # Kysytään paino
+ika_teksti = input('Kuinka vanha olet: ')  # Kysytään ikä
+sukupuoli_teksti = input(
+    'Sukupuoli mies vastaa 1 ja nainen vastaa 0: ')  # Kysytään sukupuoli
 
 # Muutetaan vastaukset luvuiksi
-pituus = float(pituus_teksti) 
-paino = float(paino_teksti) 
-ika = float(ika_teksti) 
+pituus = float(pituus_teksti)
+paino = float(paino_teksti)
+ika = float(ika_teksti)
 sukupuoli = float(sukupuoli_teksti)
 
 
@@ -27,13 +28,13 @@ def laske_bmi(paino, pituus):
     Returns:
         float: painoindeksi desimaalin tarkkuudella
     """
-    pituus = pituus / 100 # muutetaan pituus metreiksi
+    pituus = pituus / 100  # muutetaan pituus metreiksi
     bmi = paino / pituus**2
     bmi = round(bmi, 1)
     return bmi
 
-def aikuisen_rasvaprosentti(bmi, ika, sukupuoli):
 
+def aikuisen_rasvaprosentti(bmi, ika, sukupuoli):
     """_summary_
 
     Args:
@@ -49,7 +50,9 @@ def aikuisen_rasvaprosentti(bmi, ika, sukupuoli):
     rasvaprosentti = round(rasvaprosentti)
     return rasvaprosentti
 
+
 oma_bmi = laske_bmi(paino, pituus)
 oma_rasvaprosentti = aikuisen_rasvaprosentti(oma_bmi, ika, sukupuoli)
 
-print('Painoindeksisi on', oma_bmi, 'ja kehon rasvaprosentti on', oma_rasvaprosentti)
+print('Painoindeksisi on', oma_bmi,
+      'ja kehon rasvaprosentti on', oma_rasvaprosentti)
