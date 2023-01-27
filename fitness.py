e07363eab01a88f8a7222bc9eb50f65973158baa
Pyry <111853294+Pyrzenq@users.noreply.github.com>
2023-01-27 12:07:41 +0200

    asd

--- fitness.py
+++ fitness.py
@@ -1,30 +1,12 @@
 # SOVELLUS PAINOINDEKSIN JA KEHON RASVAPROSENTIN LASKEMISEEN
 # ==========================================================
 
-# Muuttujat
-
-# Kysytään käyttäjältä tiedot
-pituus_teksti = input('Kuinka pitkä olet (cm): ')  # Kysytään pituus
-paino_teksti = input('Kuinka paljon painat (kg): ')  # Kysytään paino
-ika_teksti = input('Kuinka vanha olet: ')  # Kysytään ikä
-sukupuoli_teksti = input(
-    'Sukupuoli mies vastaa 1 ja nainen vastaa 0: ')  # Kysytään sukupuoli
-
-# Muutetaan vastaukset luvuiksi
-pituus = float(pituus_teksti)
-paino = float(paino_teksti)
-ika = float(ika_teksti)
-sukupuoli = float(sukupuoli_teksti)
-
-
 # Määritellään funktio painoindeksin laskentaan
 def laske_bmi(paino, pituus):
     """Laskee painoindeksin (BMI)
-
     Args:
         paino (float): paino (kg)
         pituus (float): pituus (cm)
-
     Returns:
         float: painoindeksi desimaalin tarkkuudella
     """
@@ -34,44 +16,62 @@ def laske_bmi(paino, pituus):
     return bmi
 
 
+# Määritellään funktio aikuisen kehonrasvaprosentin laskemiseen
 def aikuisen_rasvaprosentti(bmi, ika, sukupuoli):
-    """Laskee aikuisen kehon rasvaprosentin
-
+    """_summary_
     Args:
         bmi (float): painoindeksi
         ika (float): henkilön ikä
         sukupuoli (float): 1 -> mies, 0 -> nainen
-
     Returns:
         float: kehon rasvaprosentti (aikuinen)
     """
-
     rasvaprosentti = 1.20 * bmi + 0.23 * ika - 10.8 * sukupuoli - 5.4
     rasvaprosentti = round(rasvaprosentti)
     return rasvaprosentti
 
-def lapsen_rasvaprosentti(bmi, ikä, sukupuoli):
+# Määritellään funktio lapsen kehonrasvaprosentin laskemiseen
+def lapsen_rasvaprosentti(bmi, ika, sukupuoli):
     """Laskee lapsen kehon rasvaprosentin
-
     Args:
         bmi (float): painoindeksi
-        ika (float): lapsen ikä
-        sukupuoli (float): 1 -> poika, 0 -> tyttö
-
+        ika (float): ikä
+        sukupuoli (float): poika -> 1, tyttö -> 0
     Returns:
         float: kehon rasvaprosentti (lapsi)
     """
-
     rasvaprosentti = 1.51 * bmi - 0.7 * ika - 3.6 * sukupuoli + 1.4
-    rasvaprosentti = round(rasvaprosentti)
     return rasvaprosentti
 
 
+# Suoritetaan seuraavat rivit vain, jos tämä tiedosto on pääohjelma
+# Mahdollistaa funktioiden lataamisen toisiin ohjelmiin
+# Kun koodi ladataan toiseen tiedostoon,
+# if __name__ == "__main__":n alapuolella olevaa koodia ei suoriteta
+if __name__ == "__main__":
 
-oma_bmi = laske_bmi(paino, pituus)
-oma_rasvaprosentti = aikuisen_rasvaprosentti(oma_bmi, ika, sukupuoli)
+    # Kysytään käyttäjältä tiedot
+    pituus_teksti = input('Kuinka pitkä olet (cm): ')
+    paino_teksti = input('Kuinka paljon painat (kg): ')
+    ika_teksti = input('Kuinka vanha olet: ')
+    sukupuoli_teksti = input('Sukupuoli mies, vastaa 1, nainen vastaa 0: ')
 
-print('Painoindeksisi on', oma_bmi,
-      'ja kehon rasvaprosentti on', oma_rasvaprosentti)
+    # Muutetaan vastaukset liukuluvuiksi
+    pituus = float(pituus_teksti)
+    paino = float(paino_teksti)
+    ika = float(ika_teksti)
+    sukupuoli = float(sukupuoli_teksti)
 
+    # Lasketaan painoindeksi funktiolla laske_bmi
+    oma_bmi = laske_bmi(paino, pituus)
 
+    # Yli 18 vuotiailla käytetään aikuisen kaavaa
+    if ika >= 18:
+        oma_rasvaprosentti = aikuisen_rasvaprosentti(oma_bmi, ika, sukupuoli)
+
+    # Muussa tapauksessa käytetään lapsen kaavaa
+    else:
+        oma_rasvaprosentti = lapsen_rasvaprosentti(oma_bmi, ika, sukupuoli)
+
+    print('Painoindeksisi on', oma_bmi,
+          'ja kehon rasvaprosentti on', oma_rasvaprosentti)
