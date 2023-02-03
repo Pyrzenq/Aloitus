8a3a468ead3422544d039fc787ace6b12a2c0c19
Pyry <111853294+Pyrzenq@users.noreply.github.com>
2023-02-03 12:33:52 +0200

    uuden_paivan_muutokset

--- kuntoilija.py
+++ kuntoilija.py
@@ -1,28 +1,58 @@
 # KUNTOILIJAN TIEDOT OLIO-OHJELMOINTINA
 # =====================================
 
-# KIRJASTOT JA MODULIT
-# --------------------
+# KIRJASTOT JA MODULIT (LIBRARIES AND MODULES)
+# --------------------------------------------
 
 import fitness
 
-# LUOKKAMÄÄRITYKSET
-# -----------------
-
+# LUOKKAMÄÄRITYKSET (CLASS DEFINITIONS)
+# -------------------------------------
+# Kuntoilija-luokka Yliluokka JunioriKuntoilijalle (super class)
 class Kuntoilija:
     """Luokka kuntoilijan tietoja varten"""
-    def __init__(self, nimi, pituus, paino, ika, sukupuoli):
 
+    # Olionmuodostin eli konstruktori
+    def __init__(self, nimi, pituus, paino, ika, sukupuoli):
+        
         # Määritellään tulevan olion ominaisuudet (property), luokan kentät (field)
         self.nimi = nimi
         self.pituus = pituus
         self.paino = paino
         self.ika = ika
         self.sukupuoli = sukupuoli
+        self.bmi = fitness.laske_bmi(self.paino, self.pituus)
+
+    # Metodi rasvaprosentin laskemiseen (yleinen / aikuinen)
+    def rasvaprosentti(self):
+        self.rasvaprosentti = fitness.aikuisen_rasvaprosentti(
+            self.bmi,self.ika, self.sukupuoli)
+        return self.rasvaprosentti
+
+# JunioriKuntoilija-luokka Kuntoilija-luokan aliluokka (subclass)
+class JunioriKuntoilija(Kuntoilija):
+    """Luokka nuoren kuntoilijan tiedoille"""
+    def __init__(self, nimi, pituus, paino, ika, sukupuoli):
+
+        # Määritellään periytyminen, mitä ominaisuuksia perii
+        super().__init__(nimi, pituus, paino, ika, sukupuoli)
+
+     # Metodi rasvaprosentin laskemiseen (yleinen / aikuinen)
+    def rasvaprosentti(self):
+        self.rasvaprosentti = fitness.lapsen_rasvaprosentti(
+            self.bmi, self.ika, self.sukupuoli)
+        return self.rasvaprosentti 
 
 if __name__ == "__main__":
     
-    # Luodaan olio luokasta Kuntoilija
+    # Luodaan oli luokasta Kuntoilija
     kuntoilija = Kuntoilija('Kalle Kuntoilija', 171, 65, 40, 1)
     print(kuntoilija.nimi, 'painaa', kuntoilija.paino, 'kg')
+    # print('painoindeksi on ', kuntoilija.painoindeksi())
+    print('rasvaprosentti on', kuntoilija.rasvaprosentti())
+
+    juniorikuntoilija = JunioriKuntoilija('Aku', 170, 65, 16, 1)
+    print(juniorikuntoilija.nimi, 'painaa', juniorikuntoilija.paino, 'kg')
+    # print('painoindeksi on ', kuntoilija.painoindeksi())
+    print('rasvaprosentti on', juniorikuntoilija.rasvaprosentti())
 
